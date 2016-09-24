27466b2376b58a116fb88d12bbc10f745bbc37fe
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-24 18:13:47 +0200

    Schema update maxlength added to elements

--- core/schemas/schemas.js
+++ core/schemas/schemas.js
@@ -3,10 +3,10 @@
 module.exports = {
   "users": {
     "id": {"type": "increments", "nullable": false, "primary": true},
-    "uuid": {"type": "string", "nullable": false, "unique": true},
+    "uuid": {"type": "string", "maxlength": 150, "nullable": false, "unique": true},
     "name": {"type": "string", "maxlength": 150, "nullable": false, "unique": true},
     "password": {"type": "string", "nullable": false},
-    "email": {"type": "string", "nullable": false, "unique": true},
+    "email": {"type": "string", "maxlength": 191, "nullable": false, "unique": true},
     "language": {"type": "string", "maxlength": 6, "nullable": false, "defaultTo": "en_US"},
     "last_login": {"type": "dateTime", "nullable": true},
     "created_at": {"type": "dateTime", "nullable": false},
