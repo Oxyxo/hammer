ce15576d9ff14e8deed8b665f83f5565c95be9ec
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-24 16:55:01 +0200

    Small changes to core index.js

--- core/index.js
+++ core/index.js
@@ -2,11 +2,14 @@
 
 const http = require('./http');
 const database = require('./database');
+const schemas = require('./schemas');
 
 class Core {
   constructor(config) {
-    new database(config.database);
-    new http(config.port);
+    database.open(config.database);
+    http.open(config.port);
+
+    schemas.initialize();
   }
 }
 
