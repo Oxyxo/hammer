ea54a08fc2138d566b11e2e4e2f4b0c85c4f2b93
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-24 16:54:45 +0200

    👻 HTTP open
    Can now open a http connection.
    Also checks if port is already in use.

--- core/http.js
+++ core/http.js
@@ -1,10 +1,42 @@
 'use strict';
 
-const Routes = require('./routes');
+const routes = require('./routes');
+const express = require('express');
+const net = require('net');
 
 class HTTP {
-  constructor(port) {
+  constructor() {
+    this.app = express();
+  }
+
+  open(port, cb = ()=>{}) {
+    this.portInUse(port, (inUse)=> {
+      if(inUse) {
+        //TODO: throw error port in use
+        return;
+      }
+
+      this.app.listen(port, cb);
+    });
+  }
+
+  portInUse(port, cb) {
+    let net = require('net');
+    let tester = net.createServer();
+
+    tester.once('error', (err)=> {
+      if(err.code != 'EADDRINUSE') {
+        return cb();
+      }
+      return cb(err);
+    });
+
+    tester.once('listening', ()=> {
+      tester.once('close', cb).close();
+    });
+
+    tester.listen(port);
   }
 }
 
-module.exports = HTTP;
+module.exports = new HTTP();
