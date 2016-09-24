5d946e59d56d4b417672ea625a534e0d1261cf4f
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-24 17:07:47 +0200

    Updated schemas
    added users and removed routes

--- core/schemas/schemas.js
+++ core/schemas/schemas.js
@@ -1,11 +1,17 @@
 'use strict';
 
 module.exports = {
-  "routes": {
-    "id": {"type": "increments", "nullable": false, "primary": true},
-    "url": {"type": "string", "nullable": false},
-  },
   "users": {
-    "name": {"type": "string", "nullable": false}
+    "id": {"type": "increments", "nullable": false, "primary": true},
+    "uuid": {"type": "string", "nullable": false, "unique": true},
+    "name": {"type": "string", "maxlength": 150, "nullable": false, "unique": true},
+    "password": {"type": "string", "nullable": false},
+    "email": {"type": "string", "nullable": false, "unique": true},
+    "language": {"type": "string", "maxlength": 6, "nullable": false, "defaultTo": "en_US"},
+    "last_login": {"type": "dateTime", "nullable": true},
+    "created_at": {"type": "dateTime", "nullable": false},
+    "created_by": {"type": "integer", "nullable": false},
+    "updated_at": {"type": "dateTime", "nullable": true},
+    "updated_by": {"type": "integer", "nullable": true}
   }
 };
