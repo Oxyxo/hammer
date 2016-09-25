b74ecc58225bffadc31594ff6ee39e708f902f37
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-25 11:40:21 +0200

    🛠 Database can now return models

--- core/database.js
+++ core/database.js
@@ -1,9 +1,22 @@
 const knex = require('knex');
+const bookshelf = require('bookshelf');
+const modules = require('./modules');
 
 class Database {
+  constructor() {
+
+  }
+
   open(config) {
     this.knex = knex(config);
+    this.bookshelf = bookshelf(knex);
     return this;
+  }
+
+  model(table) {
+    return this.bookshelf.Model.extend({
+      "tableName": table
+    });
   }
 }
 
