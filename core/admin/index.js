7057d95cb3951926b07ecaaacdb874a95029dc9e
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-25 17:02:59 +0200

    Moved admin to core folder

---
+++ core/admin/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const http = require('./http');
+const database = require('./database');
+const modules = require('./modules');
+
+class Admin {
+
+}
+
+module.exports = new Admin();
