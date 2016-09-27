5d16088d4d69b06cda528bd1cbf23bd5f1110fad
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-09-27 21:13:51 +0200

    🛠 Flux update

--- index.js
+++ index.js
@@ -1,15 +1,47 @@
 'use strict';
 
-const Hammer = require('./hammer');
 const core = require('./core');
-let Nail;
+const _ = require('lodash');
+const path = require('path');
+const modules = require('./core/modules');
 
-module.exports = function(config) {
-  if(Nail) {
-    return Nail;
+class Hammer {
+  constructor() {
+    global.Hammer = this;
+    return this;
   }
 
-  Nail = new Hammer(config);
-  new core(Nail.config);
-  return Nail;
-};
+  setup(config = {}) {
+    this.inputConfig = config;
+  }
+
+  get defaultConfig() {
+    return {
+      "port": 8080,
+      "database": {
+        "client": "sqlite3",
+        "connection": {
+          "filename": path.join(process.cwd(), 'database')
+        },
+        "useNullAsDefault": true
+      },
+      "hammerRoot": __dirname
+    };
+  }
+
+  set config(inputConfig = {}) {
+    this.inputConfig = inputConfig;
+  }
+
+  get config() {
+    return Object.assign(this.defaultConfig, (this.inputConfig || {}));
+  }
+
+  get modules() {
+    return modules.collect;
+  }
+
+  get plugins() {
+    //collect all plugins and return
+  }
+}
