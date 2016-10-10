8e41412edba901ef64328659cf0680acec4899e5
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-10 12:52:32 +0200

    🛠 Moved require overwrite to separate file
    Planning to in the future expand the require function to give the
    plausibility to add your own packages

---
+++ core/require.js
@@ -0,0 +1,44 @@
+'use strict';
+
+//TODO: expand module with plausibilty to add your own packages.
+
+const _ = require('lodash');
+const Module = require('module');
+const config = require('./config');
+const modules = require('./modules');
+
+class Require {
+  constructor() {
+    this.originalRequire = Module.prototype.require;
+    Module.prototype.require = this.require();
+  }
+
+  require() {
+    let self = this;
+    return function(path) {
+      let Hammer = global.Hammer;
+      let prefix = config.get.requirePrefix;
+
+      if(prefix.base.test(path)) {
+        let module = path.match(prefix.module);
+        if(module && _.isArray(module)) {
+          module = module[0];
+
+          if(Hammer.modules[module]) {
+            return Hammer.modules[module];
+          }
+
+          if(Hammer.plugins.get[module]) {
+            return Hammer.plugins.get[module];
+          }
+        }
+
+        return Hammer;
+      }
+
+      return self.originalRequire.apply(this, arguments);
+    };
+  }
+}
+
+module.exports = new Require();
--- core/utilities.js
+++ core/utilities.js
@@ -1,13 +1,11 @@
+//TODO: should this stand in a seperate file?
+
 const fs = require('fs');
 const _ = require('lodash');
-const Module = require('module');
-const config = require('./config');
-const modules = require('./modules');
 
 class Utilities {
   constructor() {
     this.mixin();
-    this.overrideRequire();
   }
 
   mixin() {
@@ -27,33 +25,6 @@ class Utilities {
     }
 
     return true;
-  }
-
-  overrideRequire() {
-    let originalRequire = Module.prototype.require;
-    let prefix = config.get.requirePrefix;
-
-    Module.prototype.require = function(path) {
-      let Hammer = global.Hammer;
-      if(prefix.base.test(path)) {
-        let module = path.match(prefix.module);
-        if(module && _.isArray(module)) {
-          module = module[0];
-
-          if(Hammer.modules[module]) {
-            return Hammer.modules[module];
-          }
-
-          if(Hammer.plugins.get[module]) {
-            return Hammer.plugins.get[module];
-          }
-        }
-
-        return Hammer;
-      }
-
-      return originalRequire.apply(this, arguments);
-    };
   }
 }
 
