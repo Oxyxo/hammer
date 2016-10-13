7f0e5b6849667f3196250232297238de1c571100
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-13 15:15:58 +0200

    🐔added the plausibility to render markdown and added the function pageData to prepare and render page data
    Also created some documentation about the render class

--- core/render.js
+++ core/render.js
@@ -1,11 +1,21 @@
 'use strict';
 
 const _ = require('lodash');
+const marked = require('marked');
 const hbs = require('handlebars');
 
+/**
+ * This function handles all render functions.
+ * Every page render goes through here.
+ *
+ * @class Render
+ */
 class Render {
   constructor() {
     this._data = [];
+    marked.setOptions({
+      breaks: true
+    });
   }
 
   serve(source) {
@@ -18,6 +28,41 @@ class Render {
 
   setDefaultRenderData(data) {
     this._data.push(data);
+  }
+
+  markdown(source) {
+    return marked(source);
+  }
+
+  /**
+   * This function prepares page data to be
+   * fed into the render function. The name of
+   * the data is defined by the stored identifier
+   *
+   * @method   Render@pageData
+   * @param    {Array} data Every object in this array needs to have the following properties: identifier, content, type. These are needed to render and set the data
+   * @return   {Object} The returned object contains the given data placed their identified properties
+   */
+  pageData(data = []) {
+    let ret = {};
+
+    for(let i=0;i<data.length;i++) {
+      let object = data[i];
+      if(!object.identifier || !object.content || !object.type) {
+        continue;
+      }
+
+      //TODO: add support for multiple content types
+      //TODO: create option for middleware/plugins to extend content types
+      switch(object.type) {
+        case 'markdown':
+          object.content = new hbs.SafeString(this.markdown(object.content));
+          break;
+      }
+      ret[object.identifier] = object.content;
+    }
+
+    return ret;
   }
 
   get renderData() {
