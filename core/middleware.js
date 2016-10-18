8c1fffb8cc814423c230bb5d511b93244e17a60f
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-18 14:02:18 +0200

    ✨ Data can now be overwritten by setting a updateData function

--- core/middleware.js
+++ core/middleware.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const _ = require('lodash');
 const async = require('async');
 
 class Middleware {
@@ -14,10 +15,12 @@ class Middleware {
     this._events[name].push(fn);
   }
 
-  call(name, args = []) {
+  call(name, args = [], opts = {}) {
     //TODO: add a function that the given args are returned with the latest changes made by middleware
     let deferred = Promise.defer(),
         promise = deferred.promise;
+
+    let {updateData} = opts;
 
     if(!Array.isArray(args)) {
       args = [args];
@@ -31,7 +34,29 @@ class Middleware {
         i++; return i < this._events[name].length;
       }, (done)=> {
         this._events[name][i].apply(null, [done].concat(args));
-      }, deferred.resolve);
+      }, (...data)=> {
+        let res;
+        if(updateData && _.isFunction(updateData)) {
+          res = args;
+          for(let i=0;i<data.length;i++) {
+            let c = data[i];
+            if(c) {
+              let newData = updateData(res, c);
+              if(newData) {
+                if(!Array.isArray(newData)) {
+                  newData = [newData];
+                }
+
+                res = newData;
+              }
+            }
+          }
+        } else {
+          res = data;
+        }
+
+        deferred.resolve(res);
+      });
     }
 
     return promise;
