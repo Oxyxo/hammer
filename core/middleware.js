1e76b3f97e9e752e4332a5a1000f94b9fb4001df
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-20 13:02:43 +0200

    🚀 Updated code flow of middleware

--- core/middleware.js
+++ core/middleware.js
@@ -19,50 +19,85 @@ class Middleware {
     let deferred = Promise.defer(),
         promise = deferred.promise;
 
-    let {updateData} = opts;
+    if(!this._events[name]) {
+      deferred.resolve();
+      return promise;
+    }
 
-    if(!Array.isArray(args)) {
+    let {dataUpdate} = opts;
+
+    if(!_.isArray(args)) {
       args = [args];
     }
 
-    let i = -1;
-    if(!this._events[name]) {
-      deferred.resolve();
-    } else {
-      async.whilst(()=> {
-        i++; return i < this._events[name].length;
-      }, (done)=> {
-        this._events[name][i].apply(null, [done].concat(args));
-      }, (...data)=> {
-        let res;
+    let i = 0;
 
-        if(updateData && _.isFunction(updateData)) {
-          res = args;
+    async.whilst(()=> {
+      return i < this._events[name].length;
+    }, (done)=> {
+      let after = (...data)=> {
+        if(dataUpdate && _.isFunction(dataUpdate)) {
+          args = dataUpdate.apply(this, [args, data]);
 
-          for(let i=0;i<data.length;i++) {
-            let c = data[i];
-
-            if(c) {
-              let newData = updateData(res, c);
-
-              if(newData) {
-
-                if(!Array.isArray(newData)) {
-                  newData = [newData];
-                }
-
-                res = newData;
-              }
-            }
+          if(!_.isArray(args)) {
+            args = [args];
           }
-
-        } else {
-          res = data;
         }
 
-        deferred.resolve(res);
-      });
-    }
+        i++;
+        done(null, data);
+      };
+
+      this._events[name][i].apply(null, [after].concat(args));
+    }, function(err, ...data) {
+      let res = data;
+      if(dataUpdate && _.isFunction(dataUpdate)) {
+        res = args;
+      }
+
+      deferred.resolve(res);
+    });
+
+    return promise;
+
+    // let i = -1;
+    // if(!this._events[name]) {
+    //   deferred.resolve();
+    // } else {
+    //   async.whilst(()=> {
+    //     i++; return i < this._events[name].length;
+    //   }, (done)=> {
+    //     this._events[name][i].apply(null, [done].concat(args));
+    //   }, (...data)=> {
+    //     let res;
+    //
+    //     if(updateData && _.isFunction(updateData)) {
+    //       res = args;
+    //
+    //       for(let i=0;i<data.length;i++) {
+    //         let c = data[i];
+    //
+    //         if(c) {
+    //           let newData = updateData(res, c);
+    //
+    //           if(newData) {
+    //
+    //             if(!_.isArray(newData)) {
+    //               newData = [newData];
+    //             }
+    //
+    //             res = newData;
+    //           }
+    //         }
+    //       }
+    //
+    //     } else {
+    //       res = data;
+    //     }
+    //
+    //     deferred.resolve(res);
+    //   });
+    // }
 
     return promise;
   }
