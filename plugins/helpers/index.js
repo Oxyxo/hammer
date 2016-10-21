76edfc4534d95e4d7ccf8f317c71b8b6efe3cdb0
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-21 15:04:18 +0200

    Updated helpers from promises to generators

--- plugins/helpers/index.js
+++ plugins/helpers/index.js
@@ -5,6 +5,7 @@ const themes = require('@hammer/themes');
 const intercom = require('@hammer/intercom');
 
 const _ = require('lodash');
+const helpers = require('@hammer/helpers');
 
 module.exports = class Helpers {
   get helpers() {
@@ -15,49 +16,47 @@ module.exports = class Helpers {
       },
       {
         "name": "header",
-        "fn": this.header()
+        "fn": this.header
       },
       {
         "name": "footer",
-        "fn": this.footer()
+        "fn": this.footer
       }
     ];
   }
 
-  include(promise, ...args) {
-    let promises = [];
+  include(...args) {
+    return function *() {
+      let promises = [];
 
-    if(1 > args.length) {
-      //TODO: optionally log here that include tag does not contain template
-      return promise.resolve();
-    }
-
-    for(let i=0;i<(args.length);i++) {
-      let arg = args[i];
-      if(!_.isString(arg)) {
-        continue;
+      if(1 > args.length) {
+        //TODO: optionally log here that include tag does not contain template
+        return;
       }
-      
-      promises.push(themes.getTemplate(arg));
-    }
 
-    Promise.all(promises).then((data)=> {
-      return promise.resolve(render.safeString(data.join('')));
-    }).catch((error)=> {
-      //TODO: log error Front end or backend?
-      return promise.resolve();
-    });
+      for(let i=0;i<(args.length);i++) {
+        let arg = args[i];
+        if(!_.isString(arg)) {
+          continue;
+        }
+
+        promises.push(themes.getTemplate(arg));
+      }
+
+      let data = yield Promise.all(promises);
+      return render.safeString(data.join(''));
+    };
   }
 
   header() {
-    return (promise)=> {
-      this.include(promise, 'header.html');
+    return function *() {
+      return yield helpers.include('header.html');
     };
   }
 
   footer() {
-    return (promise)=> {
-      this.include(promise, 'footer.html');
+    return function *() {
+      return yield helpers.include('footer.html');
     };
   }
 };
