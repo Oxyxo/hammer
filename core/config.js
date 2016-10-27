92fb466bf03d71498a56c65da5e3b6a08650118b
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-28 00:15:07 +0200

    Added documentation and changed naming of variables and functions

--- core/config.js
+++ core/config.js
@@ -1,11 +1,23 @@
 'use strict';
 
+const _ = require('lodash');
 const path = require('path');
 const render = require('./render');
 
+/**
+ * With the config class can you
+ * set and get the current set config.
+ * @class Config
+ */
 class Config {
+  /**
+   * The constructor sets the default config
+   * and sets a input object to be used
+   * later on when a new config is set or get.
+   * @constructs Config
+   */
   constructor() {
-    this._defaultConfig = {
+    this._default = {
       "port": 8080,
       "database": {
         "client": "sqlite3",
@@ -28,23 +40,62 @@ class Config {
       }
     };
 
-    this._inputConfig = {};
+    this._input = {};
   }
 
-  set set(json) {
-    this._inputConfig = Object.assign(this._inputConfig, json);
+  /**
+   * merges the given object with the already
+   * set config. Warning: The given json can
+   * override previously setted config.
+   * @param    {Object} json This object wil be merged with the current active set config.
+   * @method   Config@set
+   */
+  set set(json = {}) {
+    if(!_.isObject(json)) {
+      return;
+    }
+    this._input = Object.assign(this._input, json);
   }
 
+  /**
+   * This getter returns the current set input config.
+   * @method  Config@set
+   * @returns {Object} The current set input config.
+   */
   get set() {
-    return this._inputConfig;
+    return this._input;
   }
 
+  /**
+   * This getter returns the set config merged
+   * with the default config.
+   * @method   Config@get
+   * @returns {Object} The config object.
+   */
   get get() {
-    return Object.assign(this._defaultConfig, this._inputConfig);
+    return Object.assign(this._default, this._input);
   }
 
-  expandDefault(json) {
-    this._defaultConfig = Object.assign(this._defaultConfig, json);
+  /**
+   * The setted object wil be overriding the
+   * default config.
+   * @method   Config@default
+   * @param    {Object} json This object wil be merged with the default config.
+   */
+  set default(json = {}) {
+    if(!_.isObject(json)) {
+      return;
+    }
+    this._default = Object.assign(this._default, json);
+  }
+
+  /**
+   * Returns the
+   * @method   default
+   * @callback {[type]} [description]
+   */
+  get default() {
+    return this._default;
   }
 }
 
