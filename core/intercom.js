7f146c842ca96f453a3bce863dfea136275370fe
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-27 09:32:52 +0200

    Added documentation for intercom

--- core/intercom.js
+++ core/intercom.js
@@ -1,28 +1,60 @@
 'use strict';
 
+//TODO: add a way to change string path's to regex in order to use wildcards to listen to events.
+
+/**
+ * The intercom class gives plugins or core
+ * modules the plausibility to send/listen to socket
+ * like events.
+ * @class Intercom
+ */
 class Intercom {
+  /**
+   * This constructor creates the variable
+   * _events to store all functions that are
+   * listening to spesific events.
+   * @constructs Intercom
+   */
   constructor() {
     this._events = {};
   }
 
-  on(e, cb) {
-    if(!this._events[e]) {
-      this._events[e] = [];
+  /**
+   * By calling this function can you add
+   * a function to listen to a spesific event.
+   * This function is called when the event
+   * occures. When a callback function is called
+   * is the data stored in the arguments set by the caller.
+   * @method   Intercom@on
+   * @param    {String} event event name that you want to listen to.
+   * @param    {Function} fn this callback function is called when the event occures.
+   */
+  on(event, fn) {
+    if(!this._events[event]) {
+      this._events[event] = [];
     }
-    this._events[e].push(cb);
+    this._events[event].push(fn);
   }
 
-  emit(e, d = []) {
-    if(!Array.isArray(d)) {
-      d = [d];
+  /**
+   * With this function can you emit a event to all
+   * listeners. The given data will be apply'd to
+   * the listener callback functions.
+   * @method   Intercom@emit
+   * @param    {String} event Name of the event that you want to call.
+   * @param    {Array} data the given data will be given in form of a argument to all listeners
+   */
+  emit(event, data = []) {
+    if(!Array.isArray(data)) {
+      data = [data];
     }
 
-    if(!this._events[e]) {
+    if(!this._events[event]) {
       return;
     }
 
-    for(let i=0;i<this._events[e].length;i++) {
-      this._events[e][i].apply(null, d);
+    for(let i=0;i<this._events[event].length;i++) {
+      this._events[event][i].apply(null, data);
     }
   }
 }
