070446ed8272701cd2d0db0c9dcf2d5fc90eaae8
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-10-28 00:15:15 +0200

    Added documentation

--- core/index.js
+++ core/index.js
@@ -14,7 +14,21 @@ const utilities = require('./utilities');
 const middleware = require('./middleware');
 const authentication = require('./authentication');
 
+/**
+ * This class in constructed when a new
+ * Hammer instance is created. This class
+ * is globaly accasible and is used by modules
+ * as plugins and require.
+ * @class Hammer
+ */
 class Hammer {
+  /**
+   * When the constructor is called is a new
+   * database opened with the given / set config
+   * and a new http server opened. Also are the plugins
+   * initialized and are the modules extended.
+   * @constructs Hammer
+   */
   constructor(_config = {}) {
     let deferred = Promise.defer(),
         promise = deferred.promise;
@@ -35,9 +49,8 @@ class Hammer {
       deferred.resolve(this);
 
       log('hammer.running', {"port": config.get.port});
-    });
+    }); //TODO: add catch to catch errors!
 
-    //TODO: move modules extend to their own files/modules?
     modules.extend('log', log);
     modules.extend('intercom', intercom);
     modules.extend('middleware', middleware);
@@ -50,17 +63,47 @@ class Hammer {
     return promise;
   }
 
+  /**
+   * This getter retuns the set config
+   * merged with the default config in the
+   * config module.
+   * @method   Hammer@config
+   * @returns {Object} The set config.
+   */
   get config() {
     return config;
   }
 
+  /**
+   * The modules getter returns all
+   * extended modules found in the modules
+   * module.
+   * @method   Hammer@modules
+   * @returns {Object} All extended modules.
+   */
   get modules() {
     return modules.collect;
   }
 
+  /**
+   * This getter returns all active
+   * plugins found in the current Hammer
+   * instance. The plugins are collected by
+   * the plugins module.
+   * @method   Hammer@plugins
+   * @returns {Object} All active plugins.
+   */
   get plugins() {
     return plugins;
   }
 }
+
+process.on('uncaughtException', function (exception) {
+  //console.log(exception);
+});
+
+process.on('unhandledRejection', (reason, p) => {
+  //console.log("Unhandled Rejection at: Promise ", p, " reason: ", reason);
+});
 
 module.exports = Hammer;
