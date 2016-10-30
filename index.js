a6b71cf6def3ac5e3b480022910b1b64790998f7
Oxyxo <oxana_bulanova@hotmail.com>
2016-10-30 12:32:41 +0100

    ⚡️ Added documentation to index

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 'use strict';
 
 let Hammer = require('./core'),
-    App;
+    App; // In app is the current Hammer instance stored.
 
+/**
+ * With this function can you create a new Hammer instance
+ * or retrieve the current Hammer instance.
+ * @example let Hammer = require('projecthammer');
+ */
 module.exports = function(config = {}) {
   if(App) {
     return App;
   }
 
+  // If no current Hammer instance is found is a new one created.
   return new Hammer(config).then((core)=> {
     App = core;
   });
