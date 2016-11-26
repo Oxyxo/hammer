88c577e826dcae874b0411ed3c3099043c72e7de
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-11-26 19:06:40 +0100

    Added robots.txt to seo plugin

--- plugins/seo/index.js
+++ plugins/seo/index.js
@@ -14,6 +14,21 @@ class Seo {
         "templates": path.join(__dirname, 'templates')
       }
     };
+
+    this.allow = [];
+    this.disallow = [];
+    this.sitemap = null;
+  }
+
+  get routes() {
+    return [
+      {
+        "method": "get",
+        "url": "/robots.txt",
+        "fn": this.robots(),
+        "base": false
+      }
+    ];
   }
 
   get helpers() {
@@ -21,18 +36,34 @@ class Seo {
       {
         "name": "meta",
         "alias": ["seo"],
-        "fn": this.helper
+        "fn": this.meta
       }
     ];
   }
 
-  helper({data}) {
-    data = data.root;
+  meta({data}) {
+    let {root} = data;
     return function() {
       let meta = fs.readFileSync(path.join(config.get.seo.templates, 'meta.hbs'), 'utf-8');
 
       let template = render.serve(meta);
       return render.safeString(template(data));
+    };
+  }
+
+  robots() {
+    let self = this;
+    return function() {
+      let txt = fs.readFileSync(path.join(config.get.seo.templates, 'robots.hbs'), 'utf-8');
+      let template = render.serve(txt);
+
+      this.body = template({
+        "allow": self.allow,
+        "disallow": self.disallow,
+        "sitemap": self.sitemap
+      });
+
+      this.type = 'document';
     };
   }
 }
