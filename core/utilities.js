cab40b8062e2f819532a7865c43853da856b2f35
Jeroen Rinzema <ik@jeroenrinzema.nl>
2016-12-08 11:29:42 +0100

    Updated dependency mime to mime-types

--- core/utilities.js
+++ core/utilities.js
@@ -2,7 +2,7 @@
 
 const fs      = require('fs');
 const _       = require('lodash');
-const mime    = require('mime');
+const mime    = require('mime-types');
 const config  = require('./config');
 
 /**
